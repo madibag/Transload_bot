427a3c4bd6e2b0791bc1674c1fba3459fa86a8ca
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-11-14 07:12:25 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def start(update,context):
 
     context.bot.send_message(chat_id = chat_id,
                             text = '''HI {} This is File Transloader Created by @A_b_r_i_s_h_B\n
-                                      Type /help To get more Info'''.format(name),
+                                      /help To get more Info'''.format(name),
                                       parse_mode=ParseMode.HTML)
 def help(u,c):
     u.message.reply_text("Just send the Url you want To be transloaded")
