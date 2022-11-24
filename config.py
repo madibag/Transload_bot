eab55b470d1b5d610dc021fbd7914ebcd5c56365
madibag <62722024+madibag@users.noreply.github.com>
2022-11-24 12:02:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-	token = os.environ.get("TG_BOT_TOKEN", "")#"1116352147:AAFCW2thgkhzv0N6pkNDTBAU6Q88RkUGzIg"
-	TR_URL = os.environ.get("TR_URL","")
+	token = os.environ.get("TG_BOT_TOKEN", "") # Telegram Bot Token like >> "1116352147:AAFCW2thgkhzv0NutrkNDTByuAU6Q88RkUGzIg"
+	TR_URL = os.environ.get("TR_URL","") # Transloader Url like >> https://five.rapidleech.gq/, https://aws.rapidleech.gq/
